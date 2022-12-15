f3bbc701f60c1de761684eeccc05769a85adc5c8
MarianoArthur <marianocapiliku@gmail.com>
2022-12-15 11:38:08 +0100

    HistoryComponentStarted

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,76 @@
+import { HistoryContainer, HistoryList, StatusFlag } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meus historicos</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duracao</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag> Concluido</StatusFlag>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag> Concluido</StatusFlag>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag> Concluido</StatusFlag>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag> Concluido</StatusFlag>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag> Concluido</StatusFlag>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 meses</td>
+              <td>
+                <StatusFlag>Concluido</StatusFlag>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,24 @@ export function Home() {
             id="task"
             type="text"
             placeholder="Deia um nome para a sua tarefa"
+            list="task-suggestions"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projecto 1" />
+            <option value="Projecto 2" />
+            <option value="Projecto 3" />
+            <option value="Projecto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos</span>
